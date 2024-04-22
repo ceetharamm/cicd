3d1eb3370e16ee35f6f4b1c6ccae942775b6665b
Seetharamulu Maddipati <ceetharamm@gmail.com>
2024-04-22 12:46:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True,host='0.0.0.0',port=port)
